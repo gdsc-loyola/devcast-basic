13e8ad5ec4511378b30a6dfa4ff539eab9f2b622
Titia de Castro <laetitia.decastro@obf.ateneo.edu>
2020-12-01 15:07:07 +0800

    added episode duration

--- public/assets/scripts/audio.js
+++ public/assets/scripts/audio.js
@@ -2,6 +2,7 @@ var episode1 = document.getElementById("episode1");
 var episode2 = document.getElementById("episode2");
 var playBtn = document.getElementById('player-btnsymb');
 var pauseBtn = document.getElementById('paused');
+var durationText = document.getElementById('duration');
 
 function playEpisode(){
     if (document.getElementById('modal-episode-title').innerHTML == 'S01E01') {
@@ -36,5 +37,29 @@ function stopEpisode(){
     } else if (!episode2.paused) {
         episode2.pause();
         episode2.currentTime = 0;
+    }
+}
+
+function secondsToTime(seconds) {
+    var hr  = Math.floor(seconds / 3600);
+    var min = Math.floor((seconds - (hr * 3600))/60);
+    var sec = Math.floor(seconds - (hr * 3600) -  (min * 60));
+
+    if (min < 10){ 
+        min = "0" + min; 
+    }
+    
+    if (sec < 10){ 
+        sec  = "0" + sec;
+    }
+
+    return min + ':' + sec;
+}
+
+function displayDuration() {
+    if (document.getElementById('modal-episode-title').innerHTML == 'S01E01') {
+        durationText.innerHTML = secondsToTime(episode1.currentTime) + " / " + secondsToTime(episode1.duration);
+    } else if (document.getElementById('modal-episode-title').innerHTML == 'S01E02') {
+        durationText.innerHTML = secondsToTime(episode2.currentTime) + " / " + secondsToTime(episode2.duration);;
     }
 }
