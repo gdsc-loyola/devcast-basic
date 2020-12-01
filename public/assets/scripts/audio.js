f39c7eac415ee3e2bd8c0780f75d6dd3c1f89727
Titia de Castro <laetitia.decastro@obf.ateneo.edu>
2020-12-01 14:39:25 +0800

    added pause button

--- public/assets/scripts/audio.js
+++ public/assets/scripts/audio.js
@@ -1,11 +1,31 @@
 var episode1 = document.getElementById("episode1");
 var episode2 = document.getElementById("episode2");
+var playBtn = document.getElementById('player-btnsymb');
+var pauseBtn = document.getElementById('paused');
 
 function playEpisode(){
     if (document.getElementById('modal-episode-title').innerHTML == 'S01E01') {
         episode1.play();
     } else if (document.getElementById('modal-episode-title').innerHTML == 'S01E02') {
         episode2.play();
+    }
+
+    if (playBtn.style.display != 'none') {
+        playBtn.style.display = 'none';
+        pauseBtn.style.display = 'block';
+    }
+    else {
+        pauseEpisode();
+        playBtn.style.display = 'block';
+        pauseBtn.style.display = 'none';
+    }
+}
+
+function pauseEpisode(){
+    if (!episode1.paused) {
+        episode1.pause();
+    } else if (!episode2.paused) {
+        episode2.pause();
     }
 }
 
